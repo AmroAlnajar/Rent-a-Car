6ad16092df1cb3f62753c2bedc130bc53f55714f
Amro Alnajar <amro.alnajar92@gmail.com>
2019-06-20 02:28:17 +0200

    Initial commit.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rent_a_Car.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rent_a_Car.Startup))]
+namespace Rent_a_Car
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
