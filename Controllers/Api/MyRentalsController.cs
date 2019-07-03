a89329c0eb5fdd93c589449acb079104f7c212ca
Amro Alnajar <amro.alnajar92@gmail.com>
2019-07-03 17:23:29 +0200

    Users are able to see thier orders on their profile page.

---
+++ Controllers/Api/MyRentalsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using Rent_a_Car.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace Rent_a_Car.Controllers.Api
+{
+
+    public class MyRentalsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MyRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        [HttpGet]
+        public IHttpActionResult GetMyOrders()
+        {
+            var userid = User.Identity.GetUserId();
+
+            var orders = _context.Rentals.Include(m => m.Car).Where(m => m.CustomerId == userid).ToList();
+
+
+            return Ok(orders);
+        }
+    }
+}
